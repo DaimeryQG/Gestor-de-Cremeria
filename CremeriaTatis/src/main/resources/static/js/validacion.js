d6a50a979207a24eede9ca39a6ab76dd52aa828d
DaimeryQG12 <balastron4@gmail.com>
2025-02-09 16:11:20 -0600

    QA2
    xd

--- CremeriaTatis/src/main/resources/static/js/validacion.js
+++
@@ -1,8 +0,0 @@
-  // Seleccionar el formulario
-  const loginForm = document.getElementById("loginForm");
-
-  // Manejar el evento de envío del formulario
-  loginForm.addEventListener("submit", function (event) {
-    event.preventDefault(); // Evita que el formulario realice una acción por defecto
-    window.location.href = "./view/inicio.html"; // Redirige a inicio.html
-  });
